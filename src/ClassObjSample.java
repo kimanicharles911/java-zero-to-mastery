5c476751eaa63b2fab6f10f786bc8819c21f239d
kimanicharles911 <kimanicharles911@gmail.com>
2023-08-21 11:55:03 +0300

    mia Person object

--- src/ClassObjSample.java
+++ src/ClassObjSample.java
@@ -1,5 +1,19 @@
 public class ClassObjSample {
     public static void main(String[] args) {
+        // an object for the person class
+        Person alex = new Person();
+        alex.name = "Alex";
+        alex.age = 55;
+        alex.gender = "Male";
+        alex.sleep();
+
+        Person mia = new Person();
+        mia.name = "Mia";
+        mia.age = 40;
+        mia.gender = "Female";
+        mia.sleep();
+
+
 
     }
 }
