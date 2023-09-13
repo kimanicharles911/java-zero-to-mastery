1480355509675c946b96b5a8f7263f9a07f923de
kimanicharles911 <kimanicharles911@gmail.com>
2023-09-13 14:23:08 +0300

    importing a package

--- src/course/mypackageA/ClassObjSample.java
+++ src/course/mypackageA/ClassObjSample.java
@@ -1,5 +1,7 @@
 package course.mypackageA;
 
+import course.mypackageB.Person;
+
 public class ClassObjSample {
     public static void main(String[] args) {
         // an object for the person class
@@ -21,22 +23,5 @@ public class ClassObjSample {
         james.gender = "Male";
         james.sleep();
 
-    }
-}
-
-class Person {
-    String name;
-    int age;
-    String gender;
-
-    // sleep method of this person class
-    void sleep() {
-        if(age < 10){
-            System.out.println(name  + " will sleep more than 12 hours in a day.");
-        }else if(age >= 10 && age <= 50){
-            System.out.println(name + " will sleep less than 10 hours in a day.");
-        }else{
-            System.out.println(name + " will sleep an average of 10 hours in a day.");
-        }
     }
 }
---
+++ src/course/mypackageB/Person.java
@@ -0,0 +1,18 @@
+package course.mypackageB;
+
+public class Person {
+    public String name;
+    public int age;
+    public String gender;
+
+    // sleep method of this person class
+    public void sleep() {
+        if(age < 10){
+            System.out.println(name  + " will sleep more than 12 hours in a day.");
+        }else if(age >= 10 && age <= 50){
+            System.out.println(name + " will sleep less than 10 hours in a day.");
+        }else{
+            System.out.println(name + " will sleep an average of 10 hours in a day.");
+        }
+    }
+}
